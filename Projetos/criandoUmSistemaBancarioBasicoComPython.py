f3722d24082b6b8386ca2ba0eba040a034a2738b
Willian <wilsouza541@gmail.com>
2023-03-11 19:17:59 -0300

    Comentários
    Acrescentado comentários ao programa bancário básico

--- Projetos/criandoUmSistemaBancarioBasicoComPython.py
+++ Projetos/criandoUmSistemaBancarioBasicoComPython.py
@@ -9,6 +9,7 @@
 # Essa operação deve listar todos os depósitos e saques realizados na conta. No fim da listagem deve ser exibido o saldo atual da conta. Se o extrato estiver em branco, exibir a mensagem: Não foram realizadas movimentações.
 
 
+# Apresenta todas as opções de transações para o usuário
 menu = """
 ========================================
     [1] Depositar
@@ -18,62 +19,78 @@ menu = """
 ========================================
 Informe a operação desejada =>: """
 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
+saldo = 0               # Inicia com a conta zerada
+limite = 500            # Limite para saque
+extrato = ""            # Local onde ira salvar as informações de transações
+numero_saques = 0       # Inicia o numero de saques zerado
+# variável com letra maiuscula indica que é uma constante que não irá mudar limitando os saques a 3
 LIMITE_SAQUES = 3
 
 while True:
 
-    opcao = input(menu)
+    opcao = input(menu)     # lê a informação inserida pelo usuário
 
-    if opcao == "1":
+    if opcao == "1":        # se opção 1 DEPOSITO
         valor = float(input("\nInformar valor do depósito: "))
 
-        if valor > 0:
-            saldo += valor
+        if valor > 0:       # se valor positivo
+            saldo += valor  # acrescenta valor no saldo
+            # Acrescenta a informação em extrato
             extrato += f"Depósito:     R$ {valor: .2f}\n"
+            # Informação para o usuário
             print("Depósito realizado com sucesso!")
 
         else:
+            # Caso não for digitado uma informação aceitável levando em consideração que o usário so terá acesso a um teclado numérico
             print(
                 "\nNão foi possivel realizar o depósito! \nO valor informado é inválido.\n")
 
-    elif opcao == "2":
+    elif opcao == "2":      # se opção 2 SAQUE
         valor = float(input("\nInformar valor do saque: "))
 
+        # Caso o valor desejado para saque seja maior que o saldo
         excedeu_saldo = (valor > saldo)
+        # Caso o valor desejado para saque excedeu o Limite
         excedeu_limite = (valor > limite)
+        # Caso a quantidade de saques execedeu o limite
         excedeu_saques = (numero_saques >= LIMITE_SAQUES)
 
         if excedeu_saldo:
+            # Informação para o usuário caso excedeu saldo
             print("\nOperação falhou! \nVocê não possui saldo suficiente.\n")
 
         elif excedeu_limite:
+            # Informação para o usuário caso excedeu limite
             print("\nOperação falhou! \nVocê excedeu o limite permitido para saque.\n")
 
         elif excedeu_saques:
+            # Informação para o usuário caso excedeu numero de saques
             print("\nOperação falhou! \nVocê excedeu o numero de saques diário.\n")
 
-        elif valor > 0:
-            saldo -= valor
+        elif valor > 0:     # se valor positivo e passou njas outras condições
+            saldo -= valor  # subtrai o valor do saque de saldo
+            # Acrescenta a informação em extrato
             extrato += f"Saque:        R${valor: .2f}\n"
+            # Informação para o usuário
             print("Saque realizado com sucesso!")
+            # Acrescenta 1 na contagem de saques
             numero_saques += 1
 
         else:
+            # Informação para o usuário
             print("\nOperação falhou! \nO valor informado é inválido.\n")
 
-    elif opcao == "3":
+    elif opcao == "3":      # Se opção 3  EXTRATO
         print("\n=============== EXTRATO ===============")
+        # Caso não tenha realizado nenhuma transação se não apresenta todas informações salvas
         print("Não foram realizadas movimentações." if not extrato else extrato)
         print("\n----------------------------------------")
         print(f"\nSaldo:        R${saldo: .2f}")
         print("========================================")
 
-    elif opcao == "0":
+    elif opcao == "0":      # Se opção 3 SAI do sistema
         break
 
     else:
+        # caso não for digitado nenhuma das opção apresentadas
         print("\nOperação inválida, por favor tente novamente!\n")
